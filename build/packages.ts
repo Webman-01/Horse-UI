7c4bbd1eec2b7ca5c69f7a36ddf7d9cb81518b7a
Webman-01 <yyui268@qq.com>
2024-08-13 11:11:54 +0800

    T33

--- build/packages.ts
+++ build/packages.ts
@@ -1,28 +1,45 @@
 import { buildConfig } from "./utils/config";
 import path from "path";
-import { projectRoot } from "./utils/path";
+import { outDir, projectRoot } from "./utils/path";
 import { dest, parallel, series, src } from "gulp";
 import ts from "gulp-typescript";
-//专门打包hook,util
+import { createTaskName } from "./utils";
+// 专门打包 hook, util
 export const buildPackages = (dirname: string, name: string) => {
-  //打包cjs esm;ts->js
+  // 打包 cjs 和 esm; ts -> js
 
   const tasks = Object.entries(buildConfig).map(([module, config]) => {
     const output = path.resolve(dirname, config.output.name);
 
-    return series(() => {
+    // 创建构建任务
+    const buildTask = function buildTask() {
       const tsConfig = path.resolve(projectRoot, "tsconfig.json");
       const inputs = ["**/*.ts", "!gulpfile.ts", "!node_modules"];
       return src(inputs)
         .pipe(
           ts.createProject(tsConfig, {
-            declaration: true, //需要生成声明文件
+            declaration: true, // 需要生成声明文件
             strict: false,
             module: config.module,
           })()
         )
         .pipe(dest(output));
-    });
+    };
+
+    // 创建复制任务
+    const copyTask = function copyTask() {
+      // 放到 es 和 lib 的 utils 下
+      return src(`${output}/**`).pipe(
+        dest(path.resolve(outDir, config.output.name, name))
+      );
+    };
+
+    // 使用 `createTaskName` 为任务命名，并组合任务
+    return series(
+      createTaskName(`build:${dirname}`, buildTask),
+      createTaskName(`copy:${dirname}`, copyTask)
+    );
   });
+
   return parallel(...tasks);
 };
--- packages/theme-chalk/gulpfile.ts
+++ packages/theme-chalk/gulpfile.ts
@@ -5,6 +5,8 @@ import cleanCss from "gulp-clean-css";
 import path from "path";
 import { dest, series, src } from "gulp";
 function compile() {
+    console.log(2222);
+    
   const sass = gulpSass(dartSass);
   return src(path.resolve(__dirname, "./src/*.scss"))
     .pipe(sass.sync())
@@ -13,6 +15,8 @@ function compile() {
     .pipe(dest("./dist/css"));
 }
 function copyFullStyle() {
+    console.log(1111);
+    
   return src(path.resolve(__dirname, "./dist/**")).pipe(
     dest(path.resolve(__dirname, "../../dist/theme-chalk"))
   );
